c70e2595938353c54def76d041b2360294524749
Maria Perez <mfperez13@gmail.com>
2017-07-17 16:08:21 -0700

    project added

---
+++ TodoList/app/src/main/java/com/maria/perez/com/todolist/MainActivity.java
@@ -0,0 +1,13 @@
+package com.maria.perez.com.todolist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
